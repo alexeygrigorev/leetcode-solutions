33a2f6f367ca3f2acc35190275c29df26fc0f400
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-04-26 21:45:39 +0200

    tests for 152

--- leetcode/src/152_maximum_product_subarray.cpp
+++ leetcode/src/152_maximum_product_subarray.cpp
@@ -1,10 +1,29 @@
 #include <vector>
+#include <cstdio>
 
 #include "152_maximum_product_subarray.h"
 
 using namespace std;
 
+int max_product_naive(vector<int> &nums) {
+    int max_prod = 0;
+    int len = nums.size();
+
+    for (int i = 0; i < len; i++) {
+        int prod = nums[i];
+        for (int j = i + 1; j < len; j++) {
+            prod = prod * nums[j];
+            if (prod > max_prod) {
+                printf("s=%d: %d to %d\n", prod, i, j);
+                max_prod = prod;
+            }
+        }
+    }
+
+    return max_prod;
+}
+
 
 int MaximumProductSubarraySolution::maxProduct(vector<int> &nums) {
-    return 0;
+    return max_product_naive(nums);
 }
--- test/152_maximum_product_subarray_test.cpp
+++ test/152_maximum_product_subarray_test.cpp
@@ -10,3 +10,33 @@ TEST(MaximumProductSubarray, Test0) {
     int actual = solution152->maxProduct(nums);
     EXPECT_DOUBLE_EQ(actual, 6);
 }
+
+TEST(MaximumProductSubarray, Test1) {
+    vector<int> nums = { -2, 0, 1 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 0);
+}
+
+TEST(MaximumProductSubarray, Test2) {
+    vector<int> nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 960);
+}
+
+TEST(MaximumProductSubarray, Test3) {
+    vector<int> nums = { 1, -6, 2, -5, 3, -4, 4, -3, 5, -2, 6, -1 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 518400);
+}
+
+TEST(MaximumProductSubarray, Test4) {
+    vector<int> nums = { 6, -6, 1, -2, -6, 3, 1, -1, 2, -6, 6 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 2592);
+}
+
+TEST(MaximumProductSubarray, Test5) {
+    vector<int> nums = { -1, 1, -1, 2, -2, 3, -3, -4, -4, 5, -5, 6, -6 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 518400);
+}
