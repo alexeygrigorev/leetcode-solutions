db58ac776dbda5a55fa19aab18f7ce4c937b1d7f
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-04-28 13:31:11 +0200

    152 dp accepted

--- leetcode/src/152_maximum_product_subarray.cpp
+++ leetcode/src/152_maximum_product_subarray.cpp
@@ -1,17 +1,17 @@
 #include <vector>
 #include <cstdio>
-
+#include "limits.h"
 #include "152_maximum_product_subarray.h"
 
 using namespace std;
 
 int max_product_naive(vector<int> &nums) {
-    int max_prod = 0;
+    int max_prod = INT_MIN;
     int len = nums.size();
 
     for (int i = 0; i < len; i++) {
-        int prod = nums[i];
-        for (int j = i + 1; j < len; j++) {
+        int prod = 1;
+        for (int j = i; j < len; j++) {
             prod = prod * nums[j];
             if (prod > max_prod) {
                 printf("s=%d: %d to %d\n", prod, i, j);
@@ -23,7 +23,38 @@ int max_product_naive(vector<int> &nums) {
     return max_prod;
 }
 
+int max3(int a, int b, int c) {
+    return max(a, max(b, c));
+}
+
+int min3(int a, int b, int c) {
+    return min(a, min(b, c));
+}
+
+
+int max_product_dp(vector<int> &nums) {
+    int len = nums.size();
+
+    int max_global = nums[0];
+    int max_prev = nums[0];
+    int min_prev = nums[0];
+
+    for (int i = 1; i < len; i++) {
+        int num = nums[i];
+        int max_current = max3(num, num * min_prev, num * max_prev);
+        int min_current = min3(num, num * min_prev, num * max_prev);
+
+        if (max_current > max_global) {
+            max_global = max_current;
+        }
+
+        min_prev = min_current;
+        max_prev = max_current;
+    }
+
+    return max_global;
+}
 
 int MaximumProductSubarraySolution::maxProduct(vector<int> &nums) {
-    return max_product_naive(nums);
+    return max_product_dp(nums);
 }
--- test/152_maximum_product_subarray_test.cpp
+++ test/152_maximum_product_subarray_test.cpp
@@ -14,7 +14,7 @@ TEST(MaximumProductSubarray, Test0) {
 TEST(MaximumProductSubarray, Test1) {
     vector<int> nums = { -2, 0, 1 };
     int actual = solution152->maxProduct(nums);
-    EXPECT_DOUBLE_EQ(actual, 0);
+    EXPECT_DOUBLE_EQ(actual, 1);
 }
 
 TEST(MaximumProductSubarray, Test2) {
@@ -39,4 +39,28 @@ TEST(MaximumProductSubarray, Test5) {
     vector<int> nums = { -1, 1, -1, 2, -2, 3, -3, -4, -4, 5, -5, 6, -6 };
     int actual = solution152->maxProduct(nums);
     EXPECT_DOUBLE_EQ(actual, 518400);
+}
+
+TEST(MaximumProductSubarray, Test6) {
+    vector<int> nums = { 6, -3, -10, 0, 2 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 180);
+}
+
+TEST(MaximumProductSubarray, Test7) {
+    vector<int> nums = { -1, -3, -10, 0, 60 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 60);
+}
+
+TEST(MaximumProductSubarray, Test8) {
+    vector<int> nums = { -2, -3, 0, -2, -40 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, 80);
+}
+
+TEST(MaximumProductSubarray, Test9) {
+    vector<int> nums = { -2 };
+    int actual = solution152->maxProduct(nums);
+    EXPECT_DOUBLE_EQ(actual, -2);
 }
