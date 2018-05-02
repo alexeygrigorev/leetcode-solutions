6f62510963255b7dc2f9fe7c3915a912ad7be249
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-05-02 22:25:00 +0200

    006 accepted

--- leetcode/src/006_zigzag_conversion.cpp
+++ leetcode/src/006_zigzag_conversion.cpp
@@ -1,8 +1,76 @@
 #include <string>
+#include <cstring>
 #include "006_zigzag_conversion.h"
 
 using namespace std;
 
+void print_array(char *arr, int num_rows, int num_cols) {
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            char c = arr[i * num_cols + j];
+            if (c == 0) {
+                printf(".");
+            } else {
+                printf("%c", c);
+            }
+        }
+        printf("\n");
+    }
+}
+
+string zigzag_naive(string s, int num_rows) {
+    if (num_rows == 1) {
+        return s;
+    }
+
+    int n = s.length();
+    char *tmp = new char[num_rows * n];
+    memset(tmp, 0, sizeof(char) * num_rows * n);
+    print_array(tmp, num_rows, n);
+
+    int pos = 0;
+    int row = 0;
+    int col = 0;
+
+    while (pos < n) {
+        while (row < num_rows && pos < n) {
+            tmp[row * n + col] = s[pos];
+            print_array(tmp, num_rows, n);
+            row++;
+            pos++;
+        }
+
+        col++;
+        row--;
+        row--;
+
+        while (row > 0 && pos < n) {
+            tmp[row * n + col] = s[pos];
+            print_array(tmp, num_rows, n);
+            row--;
+            col++;
+            pos++;
+        }
+    }
+
+
+    string result(n, '?');
+    int cur_pos = 0;
+
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < col; j++) {
+            char c = tmp[i * n + j];
+            if (c != 0) {
+                result[cur_pos] = c;
+                cur_pos++;
+            }
+        }
+    }
+
+    delete tmp;
+    return result;
+}
+
 string ZigZagConversionSolution::convert(string s, int numRows) {
-    return s;
+    return zigzag_naive(s, numRows);
 }
--- test/006_zigzag_conversion_test.cpp
+++ test/006_zigzag_conversion_test.cpp
@@ -14,3 +14,18 @@ TEST(ZigZagConversion, Test1) {
     string actual = solution006->convert("PAYPALISHIRING", 4);
     EXPECT_EQ(actual, "PINALSIGYAHRPI");
 }
+
+TEST(ZigZagConversion, Test2) {
+    string actual = solution006->convert("PAYPALISHIRING", 1);
+    EXPECT_EQ(actual, "PAYPALISHIRING");
+}
+
+TEST(ZigZagConversion, Test3) {
+    string actual = solution006->convert("PAYPALISHIRING", 2);
+    EXPECT_EQ(actual, "PYAIHRNAPLSIIG");
+}
+
+TEST(ZigZagConversion, Test4) {
+    string actual = solution006->convert("nwnk", 1);
+    EXPECT_EQ(actual, "nwnk");
+}
