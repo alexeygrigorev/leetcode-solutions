8934bbbefb063eebb9e729202ba7fdea923ba893
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-05-06 13:05:00 +0200

    033 accepted

--- leetcode/src/033_search_in_rotated_sorted_array.cpp
+++ leetcode/src/033_search_in_rotated_sorted_array.cpp
@@ -1,73 +1,121 @@
 #include <vector>
-#include <algorithm>
 #include "033_search_in_rotated_sorted_array.h"
 
 using namespace std;
 
-int rotated_sorted_array_search_naive(vector<int>& nums, int target) {
+int find_sequential(vector<int> &nums, int target) {
     for (int i = 0; i < nums.size(); i++) {
         if (nums[i] == target) {
             return i;
         }
     }
 
-    return - 1;
+    return -1;
 }
 
-int bin_search(vector<int>& nums, int t, int start, int end) {
-    return binary_search(nums.begin() + start, nums.begin() + end, t);
+int bin_search(vector<int> &nums, int t, int start, int end) {
+    if (start >= end) {
+        return -1;
+    }
+
+    int m = start + (end - start) / 2;
+    int mid = nums[m];
+    if (mid == t) {
+        return m;
+    }
+
+    if (t < mid) {
+        return bin_search(nums, t, start, m);
+    } else {
+        return bin_search(nums, t, m + 1, end);
+    }
 }
 
-int pivoted_bin_search(vector<int>& nums, int t, int start, int end) {
-    int m = (end - start) / 2;
+int pivoted_bin_search(vector<int> &nums, int t, int start, int end) {
+    int m = start + (end - start) / 2;
     int mid = nums[m];
     if (mid == t) {
         return m;
     }
     int before = nums[m - 1];
+    if (before == t) {
+        return m - 1;
+    }
     int after = nums[m + 1];
+    if (after == t) {
+        return m + 1;
+    }
+
+    int first = nums[start];
+    if (first == t) {
+        return start;
+    }
+    int last = nums[end - 1];
+    if (last == t) {
+        return end - 1;
+    }
+
+    if (before < mid && mid < after) {
+        if (mid > first) {
+            // start..m is already sorted, pivot is not there
+            if (t > first && t < mid) {
+                return bin_search(nums, t, start, m);
+            } else {
+                return pivoted_bin_search(nums, t, m + 1, end);
+            }
+        }
+        if (mid < last) {
+            // m..end is already sorted
+            if (t > mid && t < last) {
+                return bin_search(nums, t, m + 1, end);
+            } else {
+                return pivoted_bin_search(nums, t, start, m);
+            }
+        }
+
+        // hm?
+    }
 
     if (mid > before && mid > after) {
-        // mid is the pivot
-        return bin_search(nums, t, start, m - 1);
-    }
+        // m is the pivot (the largest)
+        if (t > mid) {
+            return -1;
+        }
+        if (t < after) {
+            return -1;
+        }
 
-    if (mid < before && mid < after) {
-        // before is the pivot
-        return bin_search(nums, t, start, m - 2);
-    }
-
-    int first = nums.front();
-    int last = nums.back();
-
-    if (mid > first && mid < last) {
-        // pivot is on the right from m
-        if (t < mid) {
-            // already sorted before (on the left of) m
-            return bin_search(nums, t, start, m - 1);
+        if (t > first) {
+            return bin_search(nums, t, start, m);
         } else {
-            // pivot is after m (on the right of m)
-            return pivoted_bin_search(nums, t, m + 1, end);
+            return bin_search(nums, t, m + 1, end);
         }
     }
 
-    if (mid < first && mid < last) {
-        // pivot is on the left from m
+    if (mid < before && mid < after) {
+        // m is the smallest, m - 1 is the pivot
+        if (t > before) {
+            return -1;
+        }
         if (t < mid) {
-            // pivot is before m (on the left of m)
-            return pivoted_bin_search(nums, t, start, m - 1);
+            return -1;
+        }
+        if (t > first) {
+            return bin_search(nums, t, start, m - 1);
         } else {
-            // already sorted after (on the right of) m
-            return bin_search(nums, t, m + 1, end);
+            return bin_search(nums, t, m, end);
         }
     }
 
     throw exception();
 }
 
-int SearchRotatedSortedArraySolution::search(vector<int>& nums, int target) {
+int SearchRotatedSortedArraySolution::search(vector<int> &nums, int target) {
     if (nums.size() <= 5) {
-        return rotated_sorted_array_search_naive(nums, target);
+        return find_sequential(nums, target);
+    }
+    if (nums.front() < nums.back()) {
+        return bin_search(nums, target, 0, nums.size());
     }
     return pivoted_bin_search(nums, target, 0, nums.size());
 }
--- test/033_search_in_rotated_sorted_array_test.cpp
+++ test/033_search_in_rotated_sorted_array_test.cpp
@@ -6,15 +6,61 @@ using namespace std;
 SearchRotatedSortedArraySolution *solution033 = new SearchRotatedSortedArraySolution;
 
 TEST(SearchRotatedSortedArray, Test0) {
-    vector<int> nums = { 4,5,6,7,0,1,2 };
+    vector<int> nums = { 4, 5, 6, 7, 0, 1, 2 };
     int target = 0;
     int actual = solution033->search(nums, target);
     EXPECT_EQ(actual, 4);
 }
 
 TEST(SearchRotatedSortedArray, Test1) {
-    vector<int> nums = { 4,5,6,7,0,1,2 };
+    vector<int> nums = { 4, 5, 6, 7, 0, 1, 2 };
     int target = 3;
     int actual = solution033->search(nums, target);
     EXPECT_EQ(actual, -1);
 }
+
+TEST(SearchRotatedSortedArray, Test2) {
+    vector<int> nums = { 4, 5, 6, 7, 0, 1, 2, 3 };
+    for (int i = 0; i < nums.size(); i++) {
+        int target = nums[i];
+        int actual = solution033->search(nums, target);
+        EXPECT_EQ(actual, i);
+    }
+}
+
+TEST(SearchRotatedSortedArray, Test3) {
+    vector<int> nums = { 0, 1, 2, 3, 4, 5, 6, 7 };
+    for (int i = 0; i < nums.size(); i++) {
+        int target = nums[i];
+        int actual = solution033->search(nums, target);
+        EXPECT_EQ(actual, i);
+    }
+}
+
+int find(vector<int> v, int t) {
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == t) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+vector<int> shift(vector<int> nums, int size) {
+    vector<int> copy(nums);
+    rotate(copy.begin(), copy.begin() + size, copy.end());
+    return copy;
+}
+
+TEST(SearchRotatedSortedArray, Test4) {
+    vector<int> nums = { 0, 10, 20, 30, 40, 50, 60, 70 };
+    for (int s = 0; s < 7; s++) {
+        vector<int> nums_shifted = shift(nums, s);
+        for (int i = -10; i <= 80; i++) {
+            int expected = find(nums_shifted, i);
+            int actual = solution033->search(nums_shifted, i);
+            EXPECT_EQ(actual, expected);
+        }
+    }
+}
+
