e7adbf024afc22f1e60681599434177854000fc4
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-05-10 10:47:14 +0200

    207 recursion accepted

---
+++ leetcode/include/207_course_schedule.h
@@ -0,0 +1,8 @@
+#include <vector>
+
+using namespace std;
+
+class CourseScheduleSolution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites);
+};
---
+++ leetcode/src/207_course_schedule.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <cstdio>
+#include "207_course_schedule.h"
+
+#define NOT_VISITED 0
+#define PROCESSING 1
+#define PROCESSED 2
+
+using namespace std;
+
+bool dfs(vector<vector<int>>& graph, int n, vector<int>& visited) {
+    if (visited[n] == PROCESSING) {
+        return false;
+    }
+    visited[n] = PROCESSING;
+
+    for (int o : graph[n]) {
+        if (visited[o] != PROCESSED) {
+            bool res = dfs(graph, o, visited);
+            if (!res) {
+                return false;
+            }
+        }
+    }
+
+    visited[n] = PROCESSED;
+    return true;
+}
+
+bool dfs_loop(vector<vector<int>> graph, vector<int> visited) {
+    for (int i = 0; i < graph.size(); i++) {
+        if (visited[i] == NOT_VISITED) {
+            bool res = dfs(graph, i, visited);
+            if (!res) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+
+bool CourseScheduleSolution::canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+    vector<vector<int>> graph(numCourses);
+
+    for (int i = 0; i < prerequisites.size(); i++) {
+        pair<int, int> edge = prerequisites[i];
+        graph[edge.second].push_back(edge.first);
+    }
+
+    vector<int> visited(numCourses, NOT_VISITED);
+    return dfs_loop(graph, visited);
+}
