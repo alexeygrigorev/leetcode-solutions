ff4eadabc7b6ae952ae011796ea35eacaa0dcdfc
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-05-13 10:49:59 +0200

    815 accepted

--- leetcode/src/815_bus_routes.cpp
+++ leetcode/src/815_bus_routes.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include <unordered_map>
-#include <iostream>
+
 #include "815_bus_routes.h"
 
 using namespace std;
@@ -9,27 +9,112 @@ unordered_map<int, vector<int>> inverted_index(vector<vector<int>> &routes) {
     int num_routes = routes.size();
     unordered_map<int, vector<int>> index;
 
-    for (int r = 0; r < num_routes; r++) {
-        vector<int> bus_routes = routes[r];
-        for (int i = 0; i < bus_routes.size(); i++) {
-            index[bus_routes[i]].push_back(r);
+    for (int route = 0; route < num_routes; route++) {
+        for (int station : routes[route]) {
+            index[station].push_back(route);
         }
     }
 
     return index;
 }
 
-int BusRoutesSolution::numBusesToDestination(vector<vector<int>> &routes, int S, int T) {
-    unordered_map<int, vector<int>> index = inverted_index(routes);
+vector<vector<int>> build_graph(unordered_map<int, vector<int>> &index,
+        int num_routes, int S, int T) {
+    int start_node = num_routes;
+    int end_node = num_routes + 1;
+    int num_nodes = num_routes + 2;
 
-    for (auto record : index) {
-        int station = record.first;
-        vector<int> buses = record.second;
-        if (buses.size() <= 1) {
+    vector<vector<int>> graph(num_nodes);
+
+    for (auto bus_stop : index) {
+        int station = bus_stop.first;
+        vector<int> routes = bus_stop.second;
+
+        int num_buses = routes.size();
+        if (num_buses <= 1) {
             continue;
         }
 
+        for (int i : routes) {
+            for (int j : routes) {
+                if (i != j) {
+                    graph[i].push_back(j);
+                }
+            }
+        }
     }
 
-    return 0;
+    graph[start_node] = index[S];
+
+    for (int n : index[T]) {
+        graph[n].push_back(end_node);
+    }
+
+    return graph;
+}
+
+int shortest_path(vector<vector<int>> &graph, int start, int end) {
+    int num_nodes = graph.size();
+
+    vector<bool> visited(num_nodes, false);
+    vector<int> queue(num_nodes, -1);
+    vector<int> parent(num_nodes);
+
+    int q_front = 0;
+    int q_back = 0;
+
+    visited[start] = true;
+    queue[q_back] = start;
+    q_back++;
+
+    parent[start] = -1;
+
+    while (q_back > q_front) {
+        int node = queue[q_front];
+        q_front++;
+
+        for (int out : graph[node]) {
+            if (!visited[out]) {
+                visited[out] = true;
+
+                queue[q_back] = out;
+                q_back++;
+
+                parent[out] = node;
+            }
+        }
+    }
+
+    if (!visited[end]) {
+        return -1;
+    }
+
+    int path_size = 0;
+    int p = end;
+    while (p != -1) {
+        path_size++;
+        p = parent[p];
+    }
+
+    return path_size;
+}
+
+int BusRoutesSolution::numBusesToDestination(vector<vector<int>> &routes, int S, int T) {
+    if (S == T) {
+        return 0;
+    }
+
+    unordered_map<int, vector<int>> index = inverted_index(routes);
+
+    int num_routes = routes.size();
+    int start_node = num_routes;
+    int end_node = num_routes + 1;
+
+    vector<vector<int>> graph = build_graph(index, num_routes, S, T);
+    int path_size = shortest_path(graph, start_node, end_node);
+
+    if (path_size == -1) {
+        return -1;
+    }
+    return path_size - 2;
 }
--- test/815_bus_routes_test.cpp
+++ test/815_bus_routes_test.cpp
@@ -13,3 +13,34 @@ TEST(BusRoutes, Test0) {
     int actual = solution815->numBusesToDestination(routes, S, T);
     EXPECT_EQ(actual, expected);
 }
+
+TEST(BusRoutes, Test1) {
+    vector<vector<int>> routes = {{1, 2, 7}};
+    int S = 1;
+    int T = 2;
+    int expected = 1;
+    int actual = solution815->numBusesToDestination(routes, S, T);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(BusRoutes, Test2) {
+    vector<vector<int>> routes = {
+        {0, 1, 2}, // 0
+        {0, 2, 3}, // 1
+        {0, 3, 4}, // 2
+        {2, 3, 5}, // 3
+        {1, 2, 6}, // 4
+        {2, 5, 6}, // 5
+        {3, 5, 7}, // 6
+        {3, 4, 7}, // 7
+        {6, 8}, // 8
+        {5, 8}, // 9
+        {7, 8}, // 10
+    };
+
+    int S = 0;
+    int T = 8;
+    int expected = 3;
+    int actual = solution815->numBusesToDestination(routes, S, T);
+    EXPECT_EQ(actual, expected);
+}
