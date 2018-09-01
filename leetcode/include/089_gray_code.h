5433bdbed04e42424a092bccbb4ede1835cf0ba0
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-09-01 20:30:17 +0200

    089 backtracking accepted

--- leetcode/include/089_gray_code.h
+++ leetcode/include/089_gray_code.h
@@ -5,5 +5,9 @@ using namespace std;
 class GrayCodeSolution {
 public:
     vector<int> gray_code_iterative(int n);
+
+    void backtrack(int k, int *bits, vector<int> &result);
+    vector<int> gray_code_backtrack(int n);
+
     vector<int> grayCode(int n);
 };
--- leetcode/src/089_gray_code.cpp
+++ leetcode/src/089_gray_code.cpp
@@ -15,6 +15,25 @@ vector<int> GrayCodeSolution::gray_code_iterative(int n) {
     return result;
 }
 
+void GrayCodeSolution::backtrack(int k, int *bits, vector<int> &result) {
+    if (k == 0) {
+        result.push_back(*bits);
+        return;
+    }
+
+    backtrack(k - 1, bits, result);
+    *bits = *bits ^ (1 << (k - 1));
+    backtrack(k - 1, bits, result);
+}
+
+vector<int> GrayCodeSolution::gray_code_backtrack(int n) {
+    // from https://leetcode.com/problems/gray-code/discuss/29880
+    int bits = 0;
+    vector<int> result;
+    backtrack(n, &bits, result);
+    return result;
+}
+
 vector<int> GrayCodeSolution::grayCode(int n) {
     return gray_code_iterative(n);
 }
