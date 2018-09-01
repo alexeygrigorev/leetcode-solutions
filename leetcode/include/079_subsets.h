a7b6b9bf9fd36594ee019725286375fde3a10ed0
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-09-01 10:46:56 +0200

    078 recursion & iteration accepted

--- leetcode/include/079_subsets.h
+++ leetcode/include/079_subsets.h
@@ -5,5 +5,8 @@ using namespace std;
 class SubsetsSolution {
 public:
     vector<vector<int>> subset_bits(vector<int> &nums);
+    void recursion(vector<vector<int>> &result, vector<int> &tmp, vector<int> &nums, int i);
+    vector<vector<int>> subset_recursion(vector<int> &nums);
+    vector<vector<int>> subset_iterative(vector<int> &nums);
     vector<vector<int>> subsets(vector<int> &nums);
 };
--- leetcode/src/079_subsets.cpp
+++ leetcode/src/079_subsets.cpp
@@ -18,6 +18,40 @@ vector<vector<int>> SubsetsSolution::subset_bits(vector<int> &nums) {
     return result;
 }
 
+void SubsetsSolution::recursion(vector<vector<int>> &result, vector<int> &tmp, vector<int> &nums, int i) {
+    vector<int> copy = tmp;
+    result.push_back(copy);
+
+    for (int j = i; j < nums.size(); j++) {
+        tmp.push_back(nums[j]);
+        recursion(result, tmp, nums, j + 1);
+        tmp.pop_back();
+    }
+}
+
+vector<vector<int>> SubsetsSolution::subset_recursion(vector<int> &nums) {
+    vector<vector<int>> result;
+    vector<int> tmp_list;
+    recursion(result, tmp_list, nums, 0);
+    return result;
+}
+
 vector<vector<int>> SubsetsSolution::subsets(vector<int> &nums) {
     return subset_bits(nums);
+}
+
+vector<vector<int>> SubsetsSolution::subset_iterative(vector<int> &nums) {
+    vector<vector<int>> result = {{}};
+
+    for (int i = 0; i < nums.size(); i++) {
+        int cur_size = result.size();
+
+        for (int j = 0; j < cur_size; j++) {
+            vector<int> copy = result[j];
+            copy.push_back(nums[i]);
+            result.push_back(copy);
+        }
+    }
+
+    return result;
 }
