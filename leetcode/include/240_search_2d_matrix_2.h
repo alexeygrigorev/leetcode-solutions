fd833805b58f66fecf9ac9dccd3b15c0ea4e8e03
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-09-10 09:33:25 +0200

    240 m*logn (faster) accepted

--- leetcode/include/240_search_2d_matrix_2.h
+++ leetcode/include/240_search_2d_matrix_2.h
@@ -6,5 +6,10 @@ class Search2DMatrix2Solution {
     bool binary_search_2d(vector<vector<int>> &matrix, int target,
             int row_start, int row_end, int col_start, int col_end);
     bool search_matrix_naive(vector<vector<int>> &matrix, int target);
+
+    bool binary_search(vector<int> &arr, int target);
+    bool binary_search_2d_linear(vector<vector<int>> &matrix, int target);
+    bool search_marix_binsearch(vector<vector<int>> &matrix, int target);
+
     bool searchMatrix(vector<vector<int>> &matrix, int target);
 };
--- leetcode/src/240_search_2d_matrix_2.cpp
+++ leetcode/src/240_search_2d_matrix_2.cpp
@@ -40,6 +40,59 @@ bool Search2DMatrix2Solution::search_matrix_naive(vector<vector<int>> &matrix, i
     return binary_search_2d(matrix, target, 0, matrix.size(), 0, matrix[0].size());
 }
 
+
+bool Search2DMatrix2Solution::binary_search(vector<int> &arr, int target) {
+    int l = 0;
+    int r = arr.size();
+
+    while (l < r) {
+        int m = l + (r - l) / 2;
+        int el = arr[m];
+        if (el == target) {
+            return true;
+        }
+
+        if (target < el) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return false;
+}
+
+bool Search2DMatrix2Solution::binary_search_2d_linear(vector<vector<int>> &matrix, int target) {
+    int row_start = 0;
+    int size = matrix.size();
+    int row_end = size - 1;
+
+    while (row_start < size && matrix[row_start].back() < target) {
+        row_start++;
+    }
+
+    while (row_end >= 0 && matrix[row_end][0] > target) {
+        row_end--;
+    }
+
+    for (int row = row_start; row <= row_end; row++) {
+        bool found = binary_search(matrix[row], target);
+        if (found) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool Search2DMatrix2Solution::search_marix_binsearch(vector<vector<int>> &matrix, int target) {
+    if (matrix.empty() || matrix[0].empty()) {
+        return false;
+    }
+
+    return binary_search_2d_linear(matrix, target);
+}
+
 bool Search2DMatrix2Solution::searchMatrix(vector<vector<int>> &matrix, int target) {
-    return search_matrix_naive(matrix, target);
+    return search_marix_binsearch(matrix, target);
 }
