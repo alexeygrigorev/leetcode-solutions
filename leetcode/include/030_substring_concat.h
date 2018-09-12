8cdf99d42011aaeebe8170c656fa67c7f1ea2d74
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-09-12 21:54:15 +0200

    030 cleaned

--- leetcode/include/030_substring_concat.h
+++ leetcode/include/030_substring_concat.h
@@ -5,15 +5,13 @@ using namespace std;
 
 class SubstringConcatSolution {
 public:
-    vector<int> findSubstring(string s, vector<string> &words);
-
-    vector<vector<int>> calculate_matches(const string &s, const vector<string> &words) const;
-
+    int total_length(vector<string> &words);
+    pair<vector<string>, vector<int>> unique(vector<string> &words);
+    vector<vector<int>> calculate_matches(string &s, vector<string> &words);
     vector<int> find_matches(vector<vector<int>> &matches, vector<int> &lens,
                              vector<int> &counts);
-
     bool match(int start, vector<vector<int>> &matches, vector<int> &lens,
                                     vector<int> &counts, int num_used);
 
-    pair<vector<string>, vector<int>> unique(vector<string> &words);
+    vector<int> findSubstring(string s, vector<string> &words);
 };
--- leetcode/src/030_substring_concat.cpp
+++ leetcode/src/030_substring_concat.cpp
@@ -2,70 +2,34 @@
 #include <unordered_set>
 #include <unordered_map>
 
+int SubstringConcatSolution::total_length(vector<string> &words) {
+    int total_len = 0;
+    for (const string &word : words) {
+        int len = word.length();
+        total_len = total_len + len;
+    }
+    return total_len;
+}
+
 pair<vector<string>, vector<int>> SubstringConcatSolution::unique(vector<string> &words) {
-    vector<int> counts;
+    unordered_map<string, int> counter;
+
+    for (const string &w : words) {
+        counter[w]++;
+    }
+
     vector<string> unique;
+    vector<int> counts;
 
-    unordered_map<string, int> ind;
-    int pos = 0;
-
-    for (string w : words) {
-        if (ind.find(w) == ind.end()) {
-            unique.push_back(w);
-            counts.push_back(1);
-            ind[w] = pos;
-            pos++;
-        } else {
-            int i = ind[w];
-            counts[i]++;
-        }
+    for (auto pair : counter) {
+        unique.push_back(pair.first);
+        counts.push_back(pair.second);
     }
 
     return { unique, counts };
 }
 
-vector<int> SubstringConcatSolution::findSubstring(string s, vector<string> &words) {
-    if (s.empty() || words.empty()) {
-        return {};
-    }
-
-    int total_len = 0;
-    for (string word : words) {
-        int len = word.length();
-        total_len = total_len + len;
-    }
-
-    if (total_len > s.length()) {
-        return {};
-    }
-
-    auto words_counts = unique(words);
-    vector<string> unique_words = words_counts.first;
-    vector<int> counts = words_counts.second;
-
-//    printf("%d unique words\n", unique_words.size());
-
-    vector<int> lens;
-    for (string w : unique_words) {
-        lens.push_back(w.size());
-//        printf("%d, ", lens.back());
-    }
-//    printf("\n");
-
-    vector<vector<int>> matches = calculate_matches(s, unique_words);
-//    for (int i = 0; i < matches.size(); i++) {
-//        vector<int> match = matches[i];
-//        printf("matches at %d:", i);
-//        for (int m : match) {
-//            printf(" %d", m);
-//        }
-//        printf("\n");
-//    }
-
-    return find_matches(matches, lens, counts);
-}
-
-vector<vector<int>> SubstringConcatSolution::calculate_matches(const string &s, const vector<string> &words) const {
+vector<vector<int>> SubstringConcatSolution::calculate_matches(string &s, vector<string> &words) {
     int num_words = words.size();
 
     vector<vector<int>> matches;
@@ -118,16 +82,7 @@ vector<int> SubstringConcatSolution::find_matches(vector<vector<int>> &matches,
 
         vector<int> counts_copy = counts;
 
-//        printf("checking index %d\n", i);
-//        printf("counts:");
-//        for (int c : counts_copy) {
-//            printf(" %d", c);
-//        }
-//        printf("\n");
-
-
         if (match(i, matches, lens, counts_copy, total_cnt)) {
-//            printf("match at %d, adding to results\n", i);
             result.push_back(i);
         }
     }
@@ -135,62 +90,57 @@ vector<int> SubstringConcatSolution::find_matches(vector<vector<int>> &matches,
     return result;
 }
 
-//void intend(int cnt) {
-//    for (int i = 0; i < cnt; i++) {
-//        printf("    ");
-//    }
-//}
-
 bool SubstringConcatSolution::match(int start, vector<vector<int>> &matches,
                                     vector<int> &lens,
                                     vector<int> &counts, int num_remaining) {
-//    intend(num_used);
-//    printf("match(%d, %d)\n", start, num_used);
-    if (num_remaining == 0) {//num_used == lens.size()) {
-//        intend(num_used);
-//        printf(" - match!\n");
+    if (num_remaining == 0) {
         return true;
     }
 
     int n = matches.size();
 
     for (int w : matches[start]) {
-//        intend(num_used);
-//        printf(" - checking word %d...\n", w);
         if (counts[w] == 0) {
-//            intend(num_used);
-//            printf(" - already used word %d...\n", w);
             continue;
         }
 
         int len = lens[w];
         if (start + len > n) {
-//            intend(num_used);
-//            printf(" - len of %d is too large...\n", w);
             continue;
         }
 
-//        intend(num_used);
-//        printf(" - decrement cnt of %d, now ", w);
-
         counts[w]--;
-//        printf("%d\n", counts[w]);
 
         if (match(start + len, matches, lens, counts, num_remaining - 1)) {
-//            intend(num_used);
-//            printf(" - found a match!\n");
             return true;
         }
 
-//        intend(num_used);
-//        printf(" - increment cnt of %d, now ", w);
-
         counts[w]++;
-//        printf("%d\n", counts[w]);
     }
 
-//    intend(num_used);
-//    printf(" - didn't find anything at %d\n", start);
     return false;
 }
 
+vector<int> SubstringConcatSolution::findSubstring(string s, vector<string> &words) {
+    if (s.empty() || words.empty()) {
+        return {};
+    }
+
+    int total_len = total_length(words);
+    if (total_len > s.length()) {
+        return {};
+    }
+
+    auto words_counts = unique(words);
+    vector<string> unique_words = words_counts.first;
+    vector<int> counts = words_counts.second;
+
+    vector<int> lens;
+    for (const string &w : unique_words) {
+        lens.push_back(w.size());
+    }
+
+    vector<vector<int>> matches = calculate_matches(s, unique_words);
+
+    return find_matches(matches, lens, counts);
+}
